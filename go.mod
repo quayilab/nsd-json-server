fb5b8c3eb7ec629e2876fb02826c7a03a42fc52e
Quayilab <averpix@gmail.com>
2022-01-26 23:21:11 +0700

    ...

---
+++ go.mod
@@ -0,0 +1,3 @@
+module quayilab/nsd-json-server
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world!")
+}
+
+func getRootDir(d string) {
+
+}
+
+func loadConfig() {
+
+}
+
+func getAddress() {
+
+}
+
+func getPort() {
+
+}
+
+func getPathPrefix() {
+
+}
+
+func loadCollections() {
+
+}
